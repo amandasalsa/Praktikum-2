0ac233d6ce4eabfafdb98c6da416875ce6fb3ca4
amandasalsa <amanda_salsabilla_29rpl@student.smktelkom-mlg.sch.id>
2021-01-22 03:39:32 +0700

    Praktikum2

---
+++ src/praktikum2/Praktikum2.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package praktikum2;
+
+/**
+ *
+ * @author Manda Salsa
+ */
+public class Praktikum2 {
+
+   public static void main(String[] args) {
+        int i, j;
+        
+        for (i = 1 ; i <= 5; i++){
+        
+            for (j=1; j < i; j++){
+                System.out.print("@");
+                
+            }
+            for (j=i; j<=5; j++){
+                System.out.print(" ");
+            }
+            System.out.println();
+        }
+    }
+}
